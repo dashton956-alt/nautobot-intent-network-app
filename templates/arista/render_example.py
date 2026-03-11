42aa03267585ad9c22f05753603613070a3dcaee
Dan Ashton <dan.ashton@fujitsu.com>
2026-03-11 09:43:18 +0000

    Remove deprecated validation scripts and templates
    - Deleted `run_opa_checks.py` and `validate_schemas.py` scripts as they are no longer needed.
    - Removed associated dependencies from `requirements.txt`.
    - Deleted various Jinja2 templates for Arista and SR Linux configurations, including ACLs, BGP, EVPN, and management templates.
    - Cleaned up example variable files for both Arista and SR Linux.
    - Removed rendering example scripts for both platforms.

--- templates/arista/render_example.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-import yaml
-
-env = Environment(loader=FileSystemLoader('.'))
-vars = yaml.safe_load(open('templates/arista/example_vars.yaml'))
-tmpl = env.get_template('templates/arista/full_config.j2')
-print(tmpl.render(device=vars['device']))
--- templates/srlinux/render_example.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-import yaml
-
-env = Environment(loader=FileSystemLoader('.'))
-vars = yaml.safe_load(open('templates/srlinux/example_vars.yaml'))
-tmpl = env.get_template('templates/srlinux/full_config.j2')
-print(tmpl.render(device=vars['device']))
